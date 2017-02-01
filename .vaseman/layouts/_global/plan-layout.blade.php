a8c1dbcd083517a1fcc7cba3a9aa2cfae73689a5
Simon Asika <asika32764@gmail.com>
2017-02-01 22:14:39 +0800

    typo

--- .vaseman/layouts/_global/plan-layout.blade.php
+++ .vaseman/layouts/_global/plan-layout.blade.php
@@ -16,7 +16,7 @@
                     <li class="{{ $helper->page->isActive($path, 'plans.a.blade') }}"><a href="{{ $uri['base'] }}plans/a.html">A 微型企業啟動方案</a></li>
                     <li class="{{ $helper->page->isActive($path, 'plans.b.blade') }}"><a href="{{ $uri['base'] }}plans/b.html">B 中小企業形象方案</a></li>
                     <li class="{{ $helper->page->isActive($path, 'plans.c.blade') }}"><a href="{{ $uri['base'] }}plans/c.html">C 多功能國際化方案</a></li>
-                    <li class="{{ $helper->page->isActive($path, 'plans.d.blade') }}"><a href="{{ $uri['base'] }}plans/d.html">D 旗艦級客制化方案</a></li>
+                    <li class="{{ $helper->page->isActive($path, 'plans.d.blade') }}"><a href="{{ $uri['base'] }}plans/d.html">D 旗艦級客製化方案</a></li>
                     <li class="{{ $helper->page->isActive($path, 'plans.all.blade') }}"><a href="{{ $uri['base'] }}plans/all.html">方案比較與附加功能</a></li>
                 </ul>
             </div>
