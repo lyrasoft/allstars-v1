ef3df252eb634609aae9343107408a78465ce905
Simon Asika <asika32764@gmail.com>
2017-09-06 13:42:39 +0800

    Update cart page

--- .vaseman/entries/cart.blade.php
+++ .vaseman/entries/cart.blade.php
@@ -1,10 +1,10 @@
 ---
 layout: _global/html
-title: Joomla 購物車建置專家
+title: Joomla! 購物車網站方案
 home: false
 landing: true
 meta:
-    image: https://i.imgur.com/FQfhZh2.jpg
+    image: http://i.imgur.com/vn9C8o7.jpg
     desc: 星辰資訊是 Joomla! 網頁設計/購物車/形象網站專家，提供了與一般網頁設計公司不同的網站建置模式，您不需要付出昂貴的價格與長時間的等待，只要挑選合適的網站方案與想要的功能，我們可以快速替您設計出理想的網站，不讓事業的啟動受到拖延。
 ---
 
