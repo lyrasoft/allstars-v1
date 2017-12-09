fc37720b85e39c1598f54dcc72c1191b360c397e
Simon Asika <asika32764@gmail.com>
2017-12-09 12:26:25 +0800

    Change og image

--- .vaseman/entries/cart.blade.php
+++ .vaseman/entries/cart.blade.php
@@ -4,7 +4,7 @@ title: 開店超 Easy Joomla! 購物車網頁設計
 home: false
 landing: true
 meta:
-    image: http://i.imgur.com/vn9C8o7.jpg
+    image: https://i.imgur.com/jmGIyLu.jpg
     desc: 星辰資訊是 Joomla! 網頁設計/購物車/形象網站專家，提供了與一般網頁設計公司不同的網站建置模式，您不需要付出昂貴的價格與長時間的等待，只要挑選合適的網站方案與想要的功能，我們可以快速替您設計出理想的網站，不讓事業的啟動受到拖延。
 ---
 
